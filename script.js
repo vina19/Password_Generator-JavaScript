3af6bd660c40c0157203be8137a22f6bd58d2305
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2020-10-17 10:00:02 -0500

    Still need to fix the code to show the numbers in the password when the user want to have numbers in their password

--- script.js
+++ script.js
@@ -2,56 +2,65 @@
 let generateBtn = document.querySelector("#generate");
 
 // Create new list arrays for the password criterias.
-let specialChar = ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
-let uppercaseAlphabets = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-let lowercaseAlphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let specialChar = "!\"#$%&'()*+\,-./:;<=>?@[\\]^_`{|}~";
+let uppercaseAlphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowercaseAlphabets = "abcdefghijklmnopqrstuvwxyz";
+let numbers = ["hello"];
+  //"0123456789";
+
+let userPasswordChoice = "";
+console.log(userPasswordChoice);
 
 // Generate password function.
 function generatePassword() {
 
   // Ask the user for the password length.
-  let passLength = prompt("Please enter how long would you like your password to be (Must be between 8-128 characters): ");
-  
+  let userPassLengthInput = prompt("Please enter how long would you like your password to be (Must be between 8-128 characters): ");
+
   // Conditions if the length is not in between 8-128 characters and if it's in between 8-128.
-  if(passLength < 8 || passLength >= 128) {
+  if (userPassLengthInput < 8 || userPassLengthInput > 128) {
     alert("Password length should be in between 8-128 characters long.");
-  }else {
-
-    // Ask the user for the password criteria that they want.
-    let userSpecialChar = confirm("Would you like to include special characters in your password? Click OK(yes)/Cancel(no).");
-    let userUppercase = confirm("Would you like to include uppercase letters in your password? Click OK(yes)/Cancel(no).");
-    let userLowercase = confirm("Would you like to include lowercase letters in your password? Click OK(yes)/Cancel(no).");
-    let userNumbers = confirm("Would you like to include numbers in your password? Click OK(yes)/Cancel(no).");
-    
-    // Create a new array to be filled with the characters from the user choices.
-    let password = [];
-
-    // if the user choice is true then get the characters that user want.
-    if (userSpecialChar){
-      password = password.concat(specialChar);
-    }
-    if (userUppercase){
-      password = password.concat(uppercaseAlphabets);
-    }
-    if (userLowercase){
-      password = password.concat(lowercaseAlphabets);
-    }
-    if (userNumbers){
-      password = password.concat(numbers);
-    }
-
-    // Create a new variable for a place to the password result.
-    let pass = "";
-
-    // Looping the password array and get a random characters.
-    for(let i=0; i < passLength; i++){
-      pass += password[Math.floor(Math.random() * password.length)];
-    }
-
-    // return pass
-    return pass;
   }
+  else {
+    // Ask the user to confirm which password criteria that they want to have in their password.
+    let userSpecialCharConfirm = confirm("Would you like to include special characters in your password? Click OK(yes)/Cancel(no).");
+    let userUppercaseConfirm = confirm("Would you like to include uppercase letters in your password? Click OK(yes)/Cancel(no).");
+    let userLowercaseConfirm = confirm("Would you like to include lowercase letters in your password? Click OK(yes)/Cancel(no).");
+    let userNumbersConfirm = confirm("Would you like to include numbers in your password? Click OK(yes)/Cancel(no).");
+
+    // Condition if the user decided to not choose any of the password criteria
+    if (userSpecialCharConfirm === false && userUppercaseConfirm === false && userLowercaseConfirm === false && userNumbersConfirm === false) {
+      alert("Your password must contain at least one of the uppercase letters, lowercase letters, special characters, or numbers.");
+      generatePassword();
+    }
+
+    // if the user choice is true then add the characters that user want to the userPasswordChoice variable.
+    if (userSpecialCharConfirm) {
+      userPasswordChoice += specialChar;
+    }
+    if (userUppercaseConfirm) {
+      userPasswordChoice += uppercaseAlphabets;
+    }
+    if (userLowercaseConfirm) {
+      userPasswordChoice += lowercaseAlphabets;
+    }
+    if (userNumbersConfirm) {
+      userPasswordChoice += numbers;
+    }
+  }
+
+  // Create a new variable for a place to the password result.
+  let resultUserPassword = "";
+  console.log(resultUserPassword);
+
+  // Looping the password array and get a random characters.
+  for (let i=0; i < userPassLengthInput; i++) {
+    resultUserPassword += userPasswordChoice.charAt(Math.floor(Math.random() * userPasswordChoice.length));
+  }
+
+  // return password result to the user
+  return resultUserPassword;
+
 }
   
 // Write password to the #password input
